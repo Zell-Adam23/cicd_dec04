9eb5cebaabb356c68a22d585b86159cc8831a30c
Zell-Adam23 <adamzell1223@gmail.com>
2025-12-12 16:53:13 -0600

    Update test_app.py - added imports

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from app import add
+from app import add, subtract, multiply, division
 
 def test_add():
     assert add(5,6) == 11
