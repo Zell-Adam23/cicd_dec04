d9c524202b5ef43049fb02ffeaae887c38b7950d
Zell-Adam23 <adamzell1223@gmail.com>
2025-12-12 16:46:09 -0600

    Update test_app.py -  new tests for new functions

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,23 @@ def test_add():
     assert add(5,6) == 11
 
 def test_add2():
-    assert add(5,6) != 10    
+    assert add(5,6) != 10
+
+def test_sub():
+    assert subtract(10, 5) == 5
+
+def test_sub2():
+    assert subtract(11, 4) != 7
+
+def test_mult():
+    assert multiply(3,4) == 12
+
+def test_mult2():
+    assert multiply(2,7) == 14
+
+def test_div():
+    assert division(12,4) == 3
+
+def test_div2():
+    assert division(16, 4) != 7
+
