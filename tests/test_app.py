17d4ad72e70c18c2c090f1e3b1ecef9b30633be5
atzngk <atzngk@mst.edu>
2025-12-12 19:03:34 -0600

    added tests for boundaries

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import pytest
 from pathlib import Path
 
 root = Path(__file__).resolve().parents[1]
@@ -10,32 +11,25 @@ from app import add, subtract, multiply, division, log, square, sin, cos, sqroot
 def test_add():
     assert add(5,6) == 11
 
-def test_add2():
-    assert add(5,6) != 10
-
 def test_sub():
     assert subtract(10, 5) == 5
 
-def test_sub2():
-    assert subtract(11, 4) != 8
-
 def test_mult():
     assert multiply(3,4) == 12
-
-def test_mult2():
-    assert multiply(2,7) == 14
 
 def test_div():
     assert division(12,4) == 3
 
 def test_div2():
-    assert division(16, 4) != 7
+    with pytest.raises(ValueError):
+        division(16, 0)
 
 def test_log():
     assert log(100) == 2.0
 
 def test_log2():
-    assert log(8, 2) != 2
+    with pytest.raises(ValueError):
+        log(10, -3)
 
 def test_square():
     assert square(9) == 81
@@ -49,5 +43,13 @@ def test_sin():
 def test_sqroot():
     assert sqroot(16) == 4.0
 
+def test_sqroot2():
+    with pytest.raises(ValueError):
+        sqroot(-4)
+
 def test_percent():
     assert percent(23,100) == 23.0
+
+def test_percent2():
+    with pytest.raises(ValueError):
+        percent(44, 0)
